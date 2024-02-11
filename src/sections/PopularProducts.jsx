3660f2ae9f4913c153e82cd890a158af15ebea17
Alwazir Fitrah <alwazirf@gmail.com>
2024-02-11 22:45:15 +0700

    refactor: create components for PopularProductCard

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,5 +1,5 @@
+import { PopularProductCard } from "../components";
 import { products } from "../constants";
-import { star } from "../assets/icons";
 
 const PopularProducts = () => {
   return (
@@ -15,24 +15,8 @@ const PopularProducts = () => {
       </div>
 
       <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-6 gap-14">
-        {products.map((products) => (
-          <div key={products.name} className="flex flex-1 flex-col w-full">
-            <img
-              src={products.imgURL}
-              alt={products.name}
-              className="w-[282px] h-[282px]"
-            />
-            <div className="mt-8 flex justify-start gap-2.5">
-              <img src={star} alt="rating icon" width={24} height={24} />
-              <p>(4.5)</p>
-            </div>
-            <h3 className="mt-2 text-2xl leading-normal font-semibold font-palanquin">
-              {products.name}
-            </h3>
-            <p className="mt-2 font-semibold font-montserrat text-coral-red text-2xl leading-normal">
-              {products.price}
-            </p>
-          </div>
+        {products.map((product) => (
+          <PopularProductCard key={product.name} {...product} />
         ))}
       </div>
     </section>
